d91173635a0e88c92b6f7b8da1e3d78921d09943
Faiyaz Rafeek <fayaznahwi@gmail.com>
2020-03-23 15:57:21 +0530

    First commit

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+"use strict";
+
+fetch("https://hpb.health.gov.lk/api/get-current-statistical")
+    .then(function(resp){
+        return resp.json();
+    })
+    .then(function(datas){
+        console.log(datas);
+
+        //last updated time
+        document.getElementById("time").innerHTML='Last Update at '+datas.data.update_date_time; 
+        document.getElementById("time2").innerHTML='Last Update at '+datas.data.update_date_time; 
+        document.getElementById("time3").innerHTML='Last Update at '+datas.data.update_date_time; 
+        document.getElementById("time4").innerHTML='Last Update at '+datas.data.update_date_time; 
+        document.getElementById("time5").innerHTML='Last Update at '+datas.data.update_date_time; 
+
+
+        //local cases
+        document.getElementById("totalCases").innerHTML=datas.data.local_total_cases; 
+        document.getElementById("newCases").innerHTML=datas.data.local_new_cases;
+        document.getElementById("indHospitals").innerHTML=datas.data.local_total_number_of_individuals_in_hospitals; 
+        document.getElementById("deaths").innerHTML=datas.data.local_deaths;
+        document.getElementById("recovered").innerHTML=datas.data.local_recovered; 
+
+
+        //global cases
+        document.getElementById("gloTotalCases").innerHTML=datas.data.global_total_cases; 
+        document.getElementById("gloNewCases").innerHTML=datas.data.global_new_cases;
+        document.getElementById("gloDeaths").innerHTML=datas.data.global_deaths; 
+        document.getElementById("gloNewDeaths").innerHTML=datas.data.global_new_deaths; 
+        document.getElementById("gloRecovered").innerHTML=datas.data.global_recovered;    
+    });
